5c46c66b383debd446a73d28435388bfa2747fa6
shomacafe <shoma21912@gmail.com>
2023-10-11 17:44:44 +0900

    add ブログ記事作成フォームを追加

---
+++ src/components/Post/CreatePost.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { TextField, Button, Box, Typography, Card, CardHeader, Avatar } from '@mui/material';
+import clientApi from '../../api/client';
+import Cookies from 'js-cookie';
+import { useNavigate } from 'react-router-dom';
+
+const styles = {
+  container: {
+    padding: '2rem',
+    width: '100%',
+    maxWidth: '800px',
+    margin: 'auto',
+  },
+  header: {
+    textAlign: "center"
+  },
+  card: {
+    padding: '2rem',
+  }
+}
+
+const CreatePost = () => {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [thumbnailPreview, setThumbnailPreview] = useState(null);
+  const [thumbnail, setThumbnail] = useState();
+  const navigate = useNavigate();
+
+  const handleThumbnailChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setThumbnailPreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+
+      setThumbnail(file);
+    }
+  };
+
+  const onSubmit = async (data) => {
+    const confirmResult = window.confirm('記事を登録してよろしいですか？');
+
+    if (confirmResult) {
+      try {
+        const headers = {
+          'access-token': Cookies.get('_access_token'),
+          'client': Cookies.get('_client'),
+          'uid': Cookies.get('_uid'),
+        };
+
+        const requestData = new FormData();
+        requestData.append('post[title]', data.title);
+        requestData.append('post[body]', data.body);
+
+        if (thumbnail) {
+          requestData.append('post[image]', thumbnail);
+        }
+
+        const response = await clientApi.post('/posts', requestData, {
+          headers: headers,
+        });
+        console.log('API レスポンス', response.data)
+        navigate('/');
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  return (
+    <div style={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Card style={styles.card}>
+          <CardHeader style={styles.header} title='記事を投稿する' />
+          <TextField
+            label="タイトル"
+            {...register('title', { required: 'タイトルを入力してください。' })}
+            error={!!errors.title}
+            helperText={errors.title?.message}
+            fullWidth
+            margin="dense"
+          />
+          <TextField
+            label="本文"
+            multiline
+            rows={4}
+            {...register('body', { required: '本文を入力してください。' })}
+            error={!!errors.body}
+            helperText={errors.body?.message}
+            fullWidth
+            margin="dense"
+          />
+          <input
+            accept='image/*'
+            id='thumbnailInput'
+            type='file'
+            onChange={handleThumbnailChange}
+            style={{ display: 'none' }}
+          />
+          <label htmlFor='thumbnailInput'>
+            <Button
+              variant='contained'
+              color='primary'
+              component='span'
+            >
+              サムネイル画像を選択
+            </Button>
+          </label>
+          {thumbnailPreview && <img alt='サムネイルプレビュー' src={thumbnailPreview} />}
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            color="primary"
+            style={{ marginTop: '1rem' }}
+          >
+            投稿する
+          </Button>
+        </Card>
+      </form>
+    </div>
+  );
+};
+
+export default CreatePost;
--- src/components/Top.jsx
+++ src/components/Top.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Top = () => {
+  const navigate = useNavigate();
   return (
-    <div>Top</div>
+    <>
+      <div>Top</div>
+      <Link to={'new/post'}>記事を作成する</Link>
+    </>
   )
 }
 
